956934f70148fed0687d43df740b6bf755ca19f7
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2025-05-23 05:32:28 +0600

    test

--- frontend/bigganbondhu/src/main.tsx
+++ frontend/bigganbondhu/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import "@/styles/globals.css";
 import App from "./App.tsx";
-import "./lib/fontawesome";
 import { Provider } from "./provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
